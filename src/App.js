a2eaf11299764d671c9ab98155da2523d1a46e7a
tanabee <tanabee1986@gmail.com>
2019-08-24 14:50:45 +0900

    Initialize firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import config from 'util/config';
+
+firebase.initializeApp(config);
 
 function App() {
   return <div>test</div>;
---
+++ src/util/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHqLhRcngkf88dtHjv1yStcD6Z5Lrfm7M',
+  authDomain: 'twitter-wordcloud.firebaseapp.com',
+  databaseURL: 'https://twitter-wordcloud.firebaseio.com',
+  projectId: 'twitter-wordcloud',
+  storageBucket: 'twitter-wordcloud.appspot.com',
+};
+
+export default firebaseConfig;
