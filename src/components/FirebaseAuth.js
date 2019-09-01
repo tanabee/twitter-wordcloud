1083e6e38149b7b2fac63b7774a0bd083bdb3fcb
tanabee <tanabee1986@gmail.com>
2019-09-01 20:14:26 +0900

    Add FirebaseAuth.js

---
+++ src/components/FirebaseAuth.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/functions';
+import config from 'util/config';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+firebase.initializeApp(config);
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInOptions: [firebase.auth.TwitterAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: result => {
+      const {additionalUserInfo, credential} = result;
+      if (
+        additionalUserInfo &&
+        credential &&
+        credential.providerId === firebase.auth.TwitterAuthProvider.PROVIDER_ID
+      ) {
+        console.log('userinfo', additionalUserInfo);
+        console.log('credential', credential);
+        var getTweets = firebase.functions().httpsCallable('getTweets');
+        getTweets({
+          credential: credential,
+          userName: additionalUserInfo.profile.screen_name,
+        })
+          .then(result => {
+            console.log('result: ', result);
+          })
+          .catch(error => {
+            console.log('error: ', error);
+          });
+      }
+      return false;
+    },
+  },
+};
+
+export default function FirebaseAuth() {
+  return (
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+  );
+}
