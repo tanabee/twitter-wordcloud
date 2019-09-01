c52f2cd2166c389b53d4968e35c2c405403e2a8e
tanabee <tanabee1986@gmail.com>
2019-09-01 20:52:44 +0900

    Add image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, {useState} from 'react';
 import FirebaseAuth from 'components/FirebaseAuth';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    textAlign: 'center',
+  },
+}));
 
 function App() {
   const [tweets, setTweets] = useState([]);
+  const classes = useStyles();
 
   const onFetchedTweets = tweets => {
     console.log(tweets);
     setTweets(tweets);
   };
 
-  return <FirebaseAuth onFetchedTweets={onFetchedTweets} />;
+  return (
+    <div className={classes.root}>
+      <img src="/images/wordcloud.jpg" title="Twitter WordCloud" />
+      <FirebaseAuth onFetchedTweets={onFetchedTweets} />
+    </div>
+  );
 }
 
 export default App;
