a6dd810708aa6e033dd5a4b1268a1516fc3a7acb
tanabee <tanabee1986@gmail.com>
2019-09-08 17:21:36 +0900

    Implement word count

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {getTweets} from 'utils/ApiRequest';
-import kuromoji from 'kuromoji';
+import {analyzeWithTweets} from 'utils/MorphologicalAnalyzer';
 
 export default function Main(props) {
   const [tweets, setTweets] = useState([]);
@@ -13,15 +13,7 @@ export default function Main(props) {
       .then(result => {
         console.log('result: ', result);
         setTweets(result.data);
-        const text = '親譲りの無鉄砲で小供の時から損ばかりしている';
-        kuromoji.builder({}).build((err, tokenizer) => {
-          if (err) {
-            console.log(err);
-          } else {
-            const tokens = tokenizer.tokenize(text);
-            console.log(tokens);
-          }
-        });
+        analyzeWithTweets(result.data);
       })
       .catch(error => {
         console.log('error: ', error);
